0d17029cfb92b66cf469c444e68b75feab330ce0
PotatoGG <512006634@qq.com>
2018-02-07 10:18:51 +0800

    ...

--- build/server.js
+++ build/server.js
@@ -224,9 +224,9 @@ app.get('/getPic', function (req, res) {
                 if (files) {
                     var urls = [];
                     files.forEach(function (value) {
-                        urls.push("http://localhost:8081/static/" + targetfile + "/" + value);
+                        urls.push("http://172.20.1.146:8081/static/" + targetfile + "/" + value);
                     });
-                    res.send(urls);
+                    res.send({ result: true, message: '文件获取成功', data: urls });
                     res.end();
                 }
             });
